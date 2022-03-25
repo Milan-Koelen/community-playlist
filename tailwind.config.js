642dab46719a1e0d1e12124cad234204fe1b9ff7
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2022-03-25 21:19:07 +0100

    Tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+}
